48950c97749248366a5484b6e3a8941a45848a13
Aarul Kumar <aarulk0110@gmail.com>
2025-12-06 21:59:32 +0530

    add new file

---
+++ Arrays/SubArrays.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class SubArrays {
+    public static void printSubArrays(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i; j<arr.length; j++) {
+                System.out.print("[");
+                for(int k=i; k<=j; k++) {
+                    System.out.print(arr[k]);
+                    if(k<j) {
+                        System.out.print(",");
+                    }
+                }
+                System.out.print("] ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        printSubArrays(arr);
+    }
+}
