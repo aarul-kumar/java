4bb05ccfcbefd98594a6c2c599b0ec667e19ea56
Aarul Kumar <aarulk0110@gmail.com>
2025-12-06 23:14:34 +0530

    add new file

---
+++ Arrays/TrappingRainwater.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class TrappingRainwater {
+    public static int totalWater(int arr[]) {
+        int leftMax[] = new int[arr.length];
+        leftMax[0] = arr[0];
+        for(int i=1; i<arr.length; i++) {
+            leftMax[i] = Math.max(leftMax[i-1], arr[i]);
+        }
+
+        int rightMax[] = new int[arr.length];
+        rightMax[arr.length-1] = arr[arr.length-1];
+        for(int i=arr.length-2; i>=0; i--) {
+            rightMax[i] = Math.max(rightMax[i+1], arr[i]);
+        }
+
+        int trappedWater = 0;
+        for(int i=0; i<arr.length; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - arr[i];
+        }
+        return trappedWater;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {4,2,0,6,3,2,5};
+        System.out.println(totalWater(arr));
+    }
+}
