144caf0dc131fba6d5f9a271af6aed2f46e7be3b
Aarul Kumar <aarulk0110@gmail.com>
2025-12-06 21:01:51 +0530

    initial commit

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,16 @@
+package Arrays;
+public class LinearSearch {
+    public static int linearSearch(int arr[], int key) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50, 60, 70};
+        int key = 20;
+        System.out.println(linearSearch(arr, key));
+    }
+}
