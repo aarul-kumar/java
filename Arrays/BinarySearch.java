85959b3be40d8964ad7e4b0143f8aac07fb92c66
Aarul Kumar <aarulk0110@gmail.com>
2025-12-06 21:23:39 +0530

    add new file

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class BinarySearch {
+    public static int binarySearch(int arr[], int key) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end) {
+            int mid = (start + end)/2;
+            if(arr[mid] == key) {
+                return mid;
+            } else if(arr[mid] > key) {
+                end = mid - 1;
+            } else if(arr[mid] < key) {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50, 60, 70};
+        int key = 60;
+        System.out.println(binarySearch(arr, key));
+    }
+}
