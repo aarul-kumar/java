c8268a7e227fcad014b89f53a05a1064a1c71aeb
Aarul Kumar <aarulk0110@gmail.com>
2025-12-06 22:52:48 +0530

    modify file

--- Arrays/MaxSubArraySum.java
+++ Arrays/MaxSubArraySum.java
@@ -34,8 +34,21 @@ public class MaxSubArraySum {
         }
         return maxSum;
     }
+
+    public static int maxSubArraySum_kadaneAlgorithm(int arr[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++) {
+            currSum += arr[i];
+            if(currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
     public static void main(String[] args) {
         int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-        System.out.println(maxSubArraySum_prefixArray(arr));
+        System.out.println(maxSubArraySum_kadaneAlgorithm(arr));
     }
 }
