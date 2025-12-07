eadf9fd616e03fabccd65a0dfb6f188473bd27d6
Aarul Kumar <aarulk0110@gmail.com>
2025-12-08 03:01:30 +0530

    initial commit

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -15,7 +15,7 @@ public class InsertionSort {
     public static void main(String[] args) {
         int arr[] = {5, 4, 1, 3, 2};
         insertionSort(arr);
-        for(int i=0; i<arr.length; i++) {
+        for(int i=0; i<=arr.length-1; i++) {
             System.out.print(arr[i]+" ");
         }
     }
