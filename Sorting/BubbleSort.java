052e10ef5be53addd7165fbd11071571c41909ab
Aarul Kumar <aarulk0110@gmail.com>
2025-12-08 01:54:31 +0530

    initial commit

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -13,7 +13,7 @@ public class BubbleSort {
         }
     }
     public static void main(String[] args) {
-        int arr[] = {5, 4, 1, 3, 2, 7, -2};
+        int arr[] = {5, 4, 1, 3, 2};
         bubbleSort(arr);
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i]+" ");
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void selectionSort(int arr[]) {
+        for(int i=0; i<=arr.length-2; i++) {
+            int minPos = i;
+            for(int j=i+1; j<=arr.length-1; j++) {
+                if(arr[minPos]>arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+        selectionSort(arr);
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
