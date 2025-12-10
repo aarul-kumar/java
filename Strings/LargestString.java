a978c8cb215e4c1358ad5fe2c27c6b9fb9e5e7c9
Aarul Kumar <aarulk0110@gmail.com>
2025-12-10 08:33:00 +0530

    initial commit

---
+++ Strings/LargestString.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class LargestString {
+    public static String largestString(String fruits[]) {
+        String largest = fruits[0];
+        for(int i=0; i<fruits.length; i++) {
+            if(largest.compareTo(fruits[i])<0) {
+                largest = fruits[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        String fruits[] = {"apple", "mango", "banana"};
+        System.out.println(largestString(fruits));
+    }
+}
