c5f8d6beb864442c5bf2b10936422019dccc3026
Aarul Kumar <aarulk0110@gmail.com>
2025-12-11 23:09:28 +0530

    initial commit

---
+++ BitManipulation/GetithBit.java
@@ -0,0 +1,15 @@
+package BitManipulation;
+
+public class GetithBit {
+    public static int getithBit(int n, int i) {
+        int bitmask = 1<<i;
+        if((n&bitmask)==0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(getithBit(15, 2));
+    }
+}
