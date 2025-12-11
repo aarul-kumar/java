32178991be6f97466b70a13b81d7cc6dd01d06c6
Aarul Kumar <aarulk0110@gmail.com>
2025-12-11 22:21:28 +0530

    initial commit

---
+++ BitManipulation/BinaryOr.java
@@ -0,0 +1,10 @@
+package BitManipulation;
+
+public class BinaryOr {
+    public static int binaryOr(int a, int b) {
+        return a|b;
+    }
+    public static void main(String[] args) {
+        System.out.println(binaryOr(5, 6));
+    }
+}
