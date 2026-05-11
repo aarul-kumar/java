817d4760e6848241ef57c47bb7f78bc110f434b0
Aarul Kumar <aarulk0110@gmail.com>
2026-05-12 01:57:41 +0530

    initial commit

---
+++ Queue/CircularQueueUsingArray.java
@@ -0,0 +1,67 @@
+package Queue;
+
+public class CircularQueueUsingArray {
+    static class Queue {
+        int arr[];
+        int size;
+        int front;
+        int rear;
+
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            front = -1;
+            rear = -1;
+        }
+
+        public boolean isEmpty() {
+            return front == -1;
+        }
+
+        public boolean isFull() {
+            return (rear + 1) % size == front;
+        }
+
+        public void add(int data) {
+            if (isFull()) {
+                System.out.println("Queue is full");
+                return;
+            }
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int result = arr[front];
+            if (front == rear) {
+                front = rear = -1;
+            } else {
+                front = (front + 1) % size;
+            }
+            return result;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(3);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.remove());
+    }
+}
