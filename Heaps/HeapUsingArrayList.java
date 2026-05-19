10afaa4da6cfb1329166663d15ff9e296917d4b7
Aarul Kumar <aarulk0110@gmail.com>
2026-05-19 21:49:00 +0530

    initial commit

---
+++ Heaps/HeapUsingArrayList.java
@@ -0,0 +1,76 @@
+package Heaps;
+
+import java.util.ArrayList;
+
+public class HeapUsingArrayList {
+    static class Heap {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data) {
+            arr.add(data);
+            int child = arr.size() - 1;
+            int par = (child - 1) / 2;
+
+            while (child > 0 && arr.get(child) < arr.get(par)) {
+                int temp = arr.get(child);
+                arr.set(child, arr.get(par));
+                arr.set(par, temp);
+
+                child = par;
+                par = (child - 1) / 2;
+            }
+        }
+
+        public void print() {
+            System.out.println(arr);
+        }
+
+        public void peek() {
+            System.out.println(arr.get(0));
+        }
+
+        private void heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != i) {
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+                heapify(minIdx);
+            }
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            arr.remove(arr.size() - 1);
+
+            heapify(0);
+            return data;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Heap h = new Heap();
+        h.add(2);
+        h.add(3);
+        h.add(4);
+        h.add(5);
+        h.add(10);
+        h.add(1);
+        h.peek();
+        System.out.println(h.remove());
+    }
+}
