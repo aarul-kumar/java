2d42dabb06ddb8b5d1a5d37d01a086bc26562710
Aarul Kumar <aarulk0110@gmail.com>
2026-05-20 16:15:03 +0530

    initial commit

---
+++ Heaps/HeapSort.java
@@ -0,0 +1,42 @@
+package Heaps;
+
+public class HeapSort {
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+        for (int i = n / 2; i >= 0; i--) {
+            heapify(i, n, arr);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(0, i, arr);
+        }
+    }
+
+    public static void heapify(int i, int size, int arr[]) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int maxIdx = i;
+        if (left < size && arr[maxIdx] < arr[left]) {
+            maxIdx = left;
+        }
+        if (right < size && arr[maxIdx] < arr[right]) {
+            maxIdx = right;
+        }
+        if (maxIdx != i) {
+            int temp = arr[i];
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
+            heapify(maxIdx, size, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 3, 2, 6 };
+        heapSort(arr);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
---
+++ PriorityQueue/WeakestSoldier.java
@@ -0,0 +1,40 @@
+package PriorityQueue;
+
+import java.util.PriorityQueue;
+
+public class WeakestSoldier {
+    static class Row implements Comparable<Row> {
+        int soldiers;
+        int idx;
+
+        public Row(int soldiers, int idx) {
+            this.soldiers = soldiers;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Row r) {
+            if (this.soldiers == r.soldiers) {
+                return this.idx - r.idx;
+            } else {
+                return this.soldiers - r.soldiers;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = { { 1, 0, 0, 0 }, { 1, 1, 1, 1 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 } };
+        int k = 2;
+        PriorityQueue<Row> pq = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr[0].length; j++) {
+                count += arr[i][j];
+            }
+            pq.add(new Row(count, i));
+        }
+        for (int i = 0; i < k; i++) {
+            System.out.println("R"+pq.remove().idx);
+        }
+    }
+}
